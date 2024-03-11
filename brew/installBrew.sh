fcc15bf57639db226a7010ca349c85b1d07ae1f7
CharlesStockman <chuck.stockman@yahoo.com>
2024-03-10 22:13:24 -0400

    Cleaned up the brew directory

--- brew/installBrew.sh
+++ brew/installBrew.sh
@@ -1,3 +1,6 @@
+#
+# Install the Brew Package Manager and follow the instructions to configure for the terminal
+#
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
--- brew/installCommerical.sh
+++ brew/installCommerical.sh
@@ -1,3 +1,6 @@
+#
+# Install Commerical Software 
+#
 brew install --cask slack
 brew install --cask discord
 brew install --cask pycharm
