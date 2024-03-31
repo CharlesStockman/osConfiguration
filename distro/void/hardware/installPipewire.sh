1fa9691dfc36e1d743064675ac34defc9e7b7fbe
CharlesStockman <chuck.stockman@yahoo.com>
2024-03-31 10:35:58 -0400

    void linux : added more support for sound.  Not sure if it works yet

--- distro/void/hardware/installPipewire.sh
+++ distro/void/hardware/installPipewire.sh
@@ -6,12 +6,10 @@
 xbps-install --yes pipewire wireplumber
 
 
-mkdir -p /etc/alsa/conf.d
+mkdir -p -p /etc/pipewire/pipewire.conf.d
+ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/piepwire.conf.d 
+ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d
 
-ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
-ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-defualt.conf /etc/alsa/conf.d
-
-sed -i "s/;\ autospawn\ \= no/autospawn\ =\ no/g /etc/pulse/client.conf
 
 #
 # Need to run once in your windoww pipewire, pipewire-pulse, wireplumber
