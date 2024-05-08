6c616fcdd0082863783f5e2130aa4a2dce757ca0
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2024-05-07 23:05:39 -0400

    Update installFlatpakSoftware.sh

--- flatpak/installFlatpakSoftware.sh
+++ flatpak/installFlatpakSoftware.sh
@@ -2,6 +2,6 @@ flatpak --assumeyes install flathub org.libreoffice.LibreOffice
 flatpak --assumeyes install flathub com.jetbrains.PyCharm-Professional
 flatpak --assumeyes install flathub com.visualstudio.code 
 flatpak --assumeyes install flathub com.vscodium.codium
-flatpka --assumeyes install flathub com.brave.browser
+flatpka --assumeyes install flathub com.brave.Browser
 
 
