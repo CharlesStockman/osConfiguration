6b15f12eb35f59f1aa7d176b9d56f5c8efe23633
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2025-02-24 00:04:48 -0500

    Update installNixLinux.sh

--- nixPackageManager/installNixLinux.sh
+++ nixPackageManager/installNixLinux.sh
@@ -3,5 +3,5 @@
 # On nonsystemd systems it must be isntalled for each user on the system
 #
 
-sh <(curl -L https://nixos.org/inx/install) --no-daemon
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
