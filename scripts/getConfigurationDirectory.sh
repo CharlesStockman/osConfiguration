88130a6eca251395f0ea7424b7167ec4510c1c44
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2025-06-09 20:42:02 -0400

    Delete scripts/getConfigurationDirectory.sh

--- scripts/getConfigurationDirectory.sh
+++
@@ -1,7 +0,0 @@
-#
-# Purpose get configuration directory to configure the machine
-# 
-# Assumption : Have executed initialSetup.sh
-#
-( cd $osConfigurationDir ; git clone git clone https://github.com/CharlesStockman/osConfiguration.git )
-
