c80013f40525e63815e7c94b6c887165c8781394
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2025-06-10 00:13:19 -0400

    Create configure_and_install.sh

---
+++ distro/guix/configure_and_install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Create config directory and move configuration file into the directory:
+mkdir -p /mnt/etc
+cp config.scm /mnt/etc/config.scm
+
+guix system init /mnt/etc/config.scm /mnt
