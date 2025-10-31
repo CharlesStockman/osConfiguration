5b73fe9ff738dfbfa3e79826163eb1ca66677786
CharlesStockman <chuck.stockman@yahoo.com>
2025-10-30 23:54:09 -0400

    More changes

---
+++ attic/void/general/aliases.sh
@@ -0,0 +1,7 @@
+#
+# Alias for working with the void distribution
+#
+
+alias listPackagesOnLocalMachine="xbps-query --list-pkg"
+alias sv="status /var/service/*"
+alias updateWorld="xbps-install --sync --update --yesi"
