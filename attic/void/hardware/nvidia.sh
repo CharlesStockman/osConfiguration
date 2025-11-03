a72f2b3e4bb7d7c2e48e1b941ad299499f5029dc
CharlesStockman <49707084+CharlesStockman@users.noreply.github.com>
2025-11-02 21:36:09 -0500

    Fix nvidia module entries in nvidia.conf
    Corrected the way nvidia modules are added to nvidia.conf by using echo commands instead of direct redirection.

--- attic/void/hardware/nvidia.sh
+++ attic/void/hardware/nvidia.sh
@@ -13,10 +13,10 @@ sudo modprobe nvidia_uvm
 sudo modprobe nvidia_drm
 
 # Create nvidai.conf
-nvidia >> /etc/modules-load.d/nvidia.conf
-nvidia_modeset > /etc/modules-load.d/nvidia.conf
-nvidia_uvm > /etc/modules-load.d/nvidia.conf
-nvidia_drm > /etc/modules-load.d/nvidia.conf
+echo "nvidia" >> /etc/modules-load.d/nvidia.conf
+echo "nvidia_modeset" > /etc/modules-load.d/nvidia.conf
+echo "nvidia_uvm" > /etc/modules-load.d/nvidia.conf
+echo "nvidia_drm" > /etc/modules-load.d/nvidia.conf
 
 # Add nvidia-drm.modeset=1 if not already present
 if ! grep -q "nvidia-drm.modeset=1" /etc/default/grub; then
