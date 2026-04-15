845ad43554c13d3d5e83b067c7260f93cb3a8dcb
CharlesStockman <chuck.stockman@yahoo.com>
2026-04-14 23:11:06 -0400

    Cleaned up the test

--- scripts/findManPagesForExecutable/CommandDocumentation.py
+++ scripts/findManPagesForExecutable/CommandDocumentation.py
@@ -1,3 +1,5 @@
+import os
+
 class CommandDocumentation():
     '''
         The class's purpose is to provide methods that will allow the user to find the documentation for the class.
@@ -6,4 +8,6 @@ class CommandDocumentation():
             Associate the man page with an executable
             provide summary information for each executable.
     '''
-    pass
+
+    def get_paths_from_path_environment(self):
+      return iter(os.getenv("PATH").split(":"))
